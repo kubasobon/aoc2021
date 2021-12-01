bfdf9dbb5cfd3c369463be124293af34e0adda27
kubasobon <wtty.fool@gmail.com>
2021-12-01 11:49:01 +0100

    update init_day script

--- init_day.fish
+++ init_day.fish
@@ -4,8 +4,8 @@ if test (count $argv) -eq 1
     set -l day $argv[1]
     set -l padded_day (printf "%02d" $day)
     mkdir -p "day$padded_day"
-    pandoc --from html --to markdown "~/Downloads/Day $day - Advent of Code 2021.html" -o "day$padded_day/README.md"
-    rm "~/Downloads/Day $day - Advent of Code 2021.html"
+    pandoc --from html --to markdown "/home/$USER/Downloads/Day $day - Advent of Code 2021.html" -o "day$padded_day/README.md"
+    rm "/home/$USER/Downloads/Day $day - Advent of Code 2021.html"
 else
     echo "Usage: init_day.fish 1"
 end
