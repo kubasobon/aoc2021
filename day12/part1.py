50963a115b54ba2cb80773af0690608d186b9982
kubasobon <wtty.fool@gmail.com>
2021-12-13 10:50:46 +0100

    walker

--- day12/part1.py
+++ day12/part1.py
@@ -4,7 +4,7 @@ class Cave:
         self.tunnels = []
 
     def is_small(self):
-        return self.name.islower()
+        return self.name.islower() and self.name not in ("start", "end")
 
     def is_start(self):
         return self.name == "start"
@@ -21,6 +21,16 @@ class Cave:
 
     def __repr__(self):
         return self.name
+
+class Path:
+    def __init__(self, caves):
+        self.caves = caves
+
+    def __len__(self):
+        return len(self.caves)
+
+    def __repr__(self):
+        return " -> ".join(str(c) for c in self.caves)
 
 
 def build_cave_system(data):
@@ -53,6 +63,36 @@ def print_cave_system(start):
         to_visit = list(set(c for c in new_to_visit if c not in visited))
 
 
+def walk_cave_system(cave, path_so_far=None):
+    if path_so_far is None:
+        path_so_far = []
+    # print(f"{path_so_far} -> {cave}")
+    # breakpoint()
+    path_so_far.append(cave)
+    if cave.is_end():
+        return Path(path_so_far)
+    tunnel_paths = []
+    for t in cave.tunnels:
+        if t.is_start():
+            continue
+        if t.is_small() and t in path_so_far:
+            continue
+        tunnel_paths.append(walk_cave_system(t, path_so_far[:]))
+    return [p for p in tunnel_paths if len(p) > 0]
+
+def flatten_the_walk(walk):
+    paths = []
+    for x in walk:
+        if isinstance(x, list):
+            paths.extend(flatten_the_walk(x))
+        elif isinstance(x, Path):
+            paths.append(x)
+        else:
+            raise TypeError("Cannot flatten this")
+    return paths
+
+
+
 if __name__ == "__main__":
     test_data = [
         "start-A",
@@ -65,4 +105,7 @@ if __name__ == "__main__":
     ]
     data = test_data
     start = build_cave_system(data)
-    print_cave_system(start)
+    # print_cave_system(start)
+    walk = walk_cave_system(start)
+    for path in flatten_the_walk(walk):
+        print(path)
