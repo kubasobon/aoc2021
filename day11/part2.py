f70607a84a3f53be9f749f5c42285a62d905080f
kubasobon <wtty.fool@gmail.com>
2021-12-13 07:05:22 +0100

    day 11

---
+++ day11/part2.py
@@ -0,0 +1,109 @@
+import string
+import colorama
+
+
+__adjacency = [
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+]
+
+
+class Octopus:
+    def __init__(self, energy):
+        self.e = energy
+        self.neighbours = []
+        self.flashed_this_step = False
+
+    def inc(self) -> None:
+        self.e += 1
+
+    def flash(self) -> bool:
+        if self.e > 9:
+            self.e = 0
+            for n in self.neighbours:
+                n.inc()
+            self.flashed_this_step = True
+            return True
+        return False
+
+    def reset(self):
+        if self.flashed_this_step:
+            self.e = 0
+        self.flashed_this_step = False
+
+
+def build_cave(data: list) -> dict:
+    grid = {}
+    for y, row in enumerate(data):
+        for x, s in enumerate(row):
+            grid[(x, y)] = Octopus(int(s))
+
+    for coord, o in grid.items():
+        x, y = coord
+        for dx, dy in __adjacency:
+            nx = x + dx
+            ny = y + dy
+            try:
+                o.neighbours.append(grid[(nx, ny)])
+            except KeyError:
+                pass
+
+    return grid
+
+
+def draw_map(grid: dict) -> None:
+    max_y = max(y for _, y in grid)
+    max_x = max(x for x, _ in grid)
+    for y in range(max_y + 1):
+        for x in range(max_x + 1):
+            v = grid[(x, y)].e
+            if v == 0:
+                print(colorama.Fore.YELLOW + str(v), end=colorama.Style.RESET_ALL)
+            else:
+                print(v, end="")
+        print()
+
+
+def step(grid: dict) -> bool:
+    for o in grid.values():
+        o.inc()
+    new_flashes = 1
+    while new_flashes > 0:
+        new_flashes = sum(1 if o.flash() else 0 for o in grid.values())
+    all_flashed = all(o.flashed_this_step for o in grid.values())
+    for o in grid.values():
+        o.reset()
+    return all_flashed
+
+
+if __name__ == "__main__":
+    colorama.init()
+    test_data = [
+        "5483143223",
+        "2745854711",
+        "5264556173",
+        "6141336146",
+        "6357385478",
+        "4167524645",
+        "2176841721",
+        "6882881134",
+        "4846848554",
+        "5283751526",
+    ]
+    with open("input.txt") as f:
+        data = [l.strip(string.whitespace) for l in f]
+    grid = build_cave(data)
+    for i in range(500):
+        all_flashed = step(grid)
+        if all_flashed:
+            print(f"All octopi flashed on step {i+1}")
+            break
+
+        # draw_map(grid)
+        # breakpoint()
