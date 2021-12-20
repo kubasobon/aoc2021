b93542ab47c632ebc48dd6da63480fe88000b3bc
kubasobon <wtty.fool@gmail.com>
2021-12-20 10:58:16 +0100

    day18 p1 - tree

--- day18/part1.py
+++ day18/part1.py
@@ -1,59 +1,63 @@
 from collections import Counter
 
 
-def pair_levels(l, lvl):
-    c = Counter()
-    c[lvl] += 1
+class Node:
+    def __init__(self, parent, lvl, repr_):
+        self.parent = parent
+        self.level = lvl
+        l, r = repr_
+        self.left = l if isinstance(l, int) else Node(self, lvl + 1, l)
+        self.right = r if isinstance(r, int) else Node(self, lvl + 1, r)
 
-    for sub_l in l:
-        if isinstance(sub_l, int):
-            continue
-        sub_c = pair_levels(sub_l, lvl + 1)
-        for k, v in sub_c.items():
-            c[k] += v
+    def __repr__(self):
+        return f"[{str(self.left)}, {str(self.right)}]"
 
-    #print(l, " @", lvl, ": ", c, flush=True)
-    return c
+    def leftmost_lvl4(self):
+        if self.level >= 4:
+            return self
+        if isinstance(self.left, Node):
+            l4 = self.left.leftmost_lvl4()
+            if l4 is not None:
+                return l4
+        if isinstance(self.right, Node):
+            l4 = self.right.leftmost_lvl4()
+            if l4 is not None:
+                return l4
+        return None
 
-
-def numbers(l):
-    c = Counter()
-    for sub_l in l:
-        if isinstance(sub_l, int):
-            c[sub_l] += 1
+    def explode(self):
+        p = self.parent
+        left = True
+        right = True
+        while p:
+            if right and isinstance(p.right, int):
+                right = False
+                p.right += self.right
+            if left and isinstance(p.left, int):
+                left = False
+                p.left += self.left
+            if (not right) and (not left):
+                break
+            p = p.parent
+        if self.parent.left is self:
+            self.parent.left = 0
         else:
-            sub_c = numbers(sub_l)
-            for k, v in sub_c.items():
-                c[k] += v
-    return c
+            self.parent.right = 0
 
-
-def reduce(l):
-    pair_cond = any(x >= 4 for x in pair_levels(l, 0))
-    num_cond = any(x >= 10 for x in numbers(l))
-    while pair_cond or num_cond:
-        if pair_cond:
-            l = explode_leftmost(l, 0)
-        elif num_cond:
-            l = split_leftmost(l)
-        break
-        pair_cond = any(x >= 4 for x in pair_levels(l, 0))
-        num_cond = any(x >= 10 for x in numbers(l))
-    return l
-
-def explode_leftmost(l, lvl):
-    left, right = l
-    if isinstance(left, list):
-        print(left, lvl+1)
-        if lvl+1 < 4:
-            explode_leftmost(left, lvl+1)
-        else:
-            left = 99
-    l = [left, right]
-    return l
 
 if __name__ == "__main__":
-    data = "[[[[[9,8],1],2],3],4]"
-    l = eval(data)
-    l = reduce(l)
-    print(l)
+    data = [
+        # "[[[[[9,8],1],2],3],4]",
+        # "[7,[6,[5,[4,[3,2]]]]]",
+        # "[[6,[5,[4,[3,2]]]],1]",
+        "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]",
+        # "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]",
+    ]
+    for d in data:
+        l = eval(d)
+        tree = Node(None, 0, l)
+        print(tree, end=" -> ")
+        print(tree.leftmost_lvl4())
+        tree.leftmost_lvl4().explode()
+        print(tree)
+        print()
